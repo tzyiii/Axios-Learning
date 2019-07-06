35c1f4aff19184423936ecbe86b7d2390f7e743a
tzyiii <zhengyt@uci.edu>
2019-07-06 14:51:45 -0500

    add router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -11,7 +11,7 @@ class Blog extends Component {
     state = {
         posts: [],
         selectedPostId: null
-    }
+    };
 
     componentDidMount () {
         axios.get( 'https://jsonplaceholder.typicode.com/posts' )
@@ -34,7 +34,7 @@ class Blog extends Component {
 
     postSelectedHandler = (id) => {
         this.setState({selectedPostId: id});
-    }
+    };
 
     render () {
         const posts = this.state.posts.map(post => {
@@ -46,7 +46,15 @@ class Blog extends Component {
         });
 
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
